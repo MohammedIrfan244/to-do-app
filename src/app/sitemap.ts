db69c6df28fddaf19cb6132fc7e5aa63a6a362d2
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2026-07-12 01:40:39 +0530

    fixed sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,42 +10,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: `${siteUrl}/todo`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/calendar`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/notes`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/duria`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${siteUrl}/calculator`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    // TODO: Add the landing page route here once /about is implemented
-    // {
-    //   url: `${siteUrl}/about`,
-    //   lastModified: new Date(),
-    //   changeFrequency: "monthly",
-    //   priority: 0.9,
-    // },
   ];
 }
